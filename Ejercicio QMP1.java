d1f7489da6dd53ab92ecb21d0f4ece9ea76d565a
Paola Sabrina Ferreyra <102702825+paferreyra@users.noreply.github.com>
2024-04-11 13:04:23 -0300

    Arreglo de Prosa

--- Ejercicio QMP1.java
+++ Ejercicio QMP1.java
@@ -59,17 +59,17 @@ public enum Color {
 //EXPLICACION - PROSA
 /*
 Se crea una clase Prenda directamente ya que puede haber varias prendas en un atuendo. No una clase anterior Atuendo 
-en donde salga que tiene muchas prendas porque de momento no se especifica como se lleva a cabo el atuendo, asi que no 
-debemos centrarnos en ello porque no es el foco en este momento.
+en donde salga que tiene muchas prendas porque de momento no se especifica como se lleva a cabo el atuendo o como generarlo, 
+asi que no debemos tomarlo en cuenta porque no es el foco del requerimiento y ademas no debe quedar una clase sin comportamiento.
 Primero se tienen que validar las cuestiones de la prenda para crear el autendo.
-En la clase prenda se componen sus atributos sin ser Strings porque los String si se necesitan comparar luego
-o usarlos en otra instancia y se escriben con un solo caracter distinto rompe.
-Para el tipo de la prenda se creo una clase TipoPrenda en donde allí se engloba tambien la categoria porque se
-ve conveniente que el tipo de prenda este en conjunto con su categoria para aportar robustez sin requerir a tantas
-validaciones if en el camino, ya que se debe contemplar con cada tipo de prenda a que categoria del cuerpo pertenece, asi 
-que alli fueron creados algunos objetos TipoPrenda como ejemplo con sus respectivas categorias.
-Luego los materiales y los colores fueron diseñados con enums para mas sencilles.
-Por ultimo siguiendo en la clase Prenda, se hace la respectiva validacion de si la prenda es valida o no segun si se han puesto
-los atributos necesarios. Si no se han puesto los atributos necesarios se valida con una excepcion de PrendaNoApta para crear
-el atuendo.
+En la clase prenda se componen sus atributos (tipo,material,colores) sin ser Strings porque los String, si se necesitan comparar luego
+o usarlos en otra instancia, se pueden escribir con un error en un solo caracter y rompe.
+Para el tipo de la prenda se creo una clase TipoPrenda (son varios tipos) en donde allí se engloba tambien la categoria de los mismos
+porque se ve conveniente que el tipo de prenda este en conjunto con su categoria para aportar robustez y abstaccion sin requerir a tantas
+validaciones if en el camino (donde lo convertiria en codigo mas algoritmico/iperativo), ya que se debe contemplar con cada tipo de prenda 
+a que categoria del cuerpo pertenece, asi que alli fueron creados algunos objetos TipoPrenda como ejemplo con sus respectivas categorias.
+Luego los materiales y los colores fueron diseñados con enums para mas simplicidad (aunque no se toman en cuenta todas las posibilidades
+pero en el requirimiento no aclara).
+Por ultimo siguiendo en la clase Prenda, se hace la respectiva validacion de si la prenda es valida o no segun si se han puesto todos los
+los atributos necesarios. Si no se han puesto los atributos necesarios se valida con una excepcion de PrendaNoApta para crear el atuendo.
 */
